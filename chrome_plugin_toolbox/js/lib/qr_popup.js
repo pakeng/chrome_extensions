cf0d0ac0113a57821bb8f9e53e88ce303b88d654
daipeng <625125312@qq.com>
2018-10-09 23:03:35 +0800

    新建toolbox工程

---
+++ chrome_plugin_toolbox/js/lib/qr_popup.js
@@ -0,0 +1,28 @@
+function $_id(id) {
+    return document.getElementById(id);
+}
+
+function show(dom) {
+    dom.style.display = "block";
+}
+
+function hide(dom) {
+    dom.style.display = "none";
+}
+
+function convertImgToBase64(url, callback, outputFormat){
+    var canvas = document.createElement('CANVAS'),
+        ctx = canvas.getContext('2d'),
+        img = new Image;
+    img.crossOrigin = 'Anonymous';
+    img.onload = function(){
+        canvas.height = img.height;
+        canvas.width = img.width;
+        ctx.drawImage(img,0,0);
+        var dataURL = canvas.toDataURL(outputFormat || 'image/png');
+        callback.call(this, dataURL);
+        canvas = null; 
+    };
+    img.src = url;
+}
+
